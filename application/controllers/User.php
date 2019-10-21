c45825ab9a3bf64fdf36d6afc93d68fcc72ac133
nikpatel <tp007143007@gmail.com>
2019-10-21 08:27:39 +0530

    Updated

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,8 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends MY_Controller {
-	public function index()
-	{
+	public function index()	{
 		$this->load->view('welcome_message');
 	}
 }
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,6 +1,8 @@
 <?php
 
 class MY_Controller extends CI_Controller{
-
+    public function __construct(){
+        
+    }
 
 }
