1db478a70da34ab880d63aa9df6ecb2357397196
nikpatel <tp007143007@gmail.com>
2019-10-21 08:26:27 +0530

    first commit

---
+++ application/views/admin/admin_login.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Admin Login</title>
+	<?= link_tag('assets/css/bootstrap/bootstrap.min.css') ?>
+</head>
+<body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+</body>
+</html>
